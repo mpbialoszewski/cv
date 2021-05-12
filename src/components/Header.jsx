c6a92bbb61bbf0283081062adb5f77148863a0a0
Mariusz <mab152@aber.ac.uk>
2021-05-12 23:12:02 +0100

    Adding typedReact component, really cool

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
+import Typed from 'react-typed'
 
 const Header = () => {
     return (
         <div>
            <div className="header-wrapper">
                <div className="main-info">
-                   <h1>Something</h1>
+                   <h1>I work with such technologies as:</h1>
+                   <Typed 
+                    className='typed-text'
+                    strings={['Ruby on Rails','React.js', 'Node.js']}
+                    typeSpeed={70}
+                    backSpeed={80} 
+                    loop                  
+                   />
                </div>
                </div> 
         </div>
