7e9a738d1be307e32ac65de4887d2be8f3b969fb
Mariusz <mab152@aber.ac.uk>
2021-05-12 22:44:20 +0100

    Changing logo for hamburger menu

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,14 +1,17 @@
 import React from "react"
 import logo from "../logo.svg"
+//ReactFontAwesome imports
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faBars} from '@fortawesome/free-solid-svg-icons'
 
-export const Navbar = () => {
+const Navbar = () => {
     return (
-<>
+      <>
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
         <div className="container">
         <a className="navbar-brand" href="#"><img src={logo} alt="logo..."></img>mpbialoszewski</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
+          <FontAwesomeIcon icon={faBars} style={{color: 'var(--cvOrange)'}}/>
         </button>
       
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
