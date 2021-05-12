4dc1709d58deaf462f6e3a8eb46eaaed955de315
Mariusz <mab152@aber.ac.uk>
2021-05-12 23:02:59 +0100

    Adding header

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+           <div className="header-wrapper">
+               <div className="main-info">
+                   <h1>Something</h1>
+               </div>
+               </div> 
+        </div>
+    )
+}
+
+export default Header;
