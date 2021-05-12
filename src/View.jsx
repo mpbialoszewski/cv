efb35a704181716e40ad9eaa424dfa0893583f89
Mariusz <mab152@aber.ac.uk>
2021-05-12 18:32:04 +0100

    New commit

---
+++ src/View.jsx
@@ -0,0 +1,15 @@
+import react from 'react';
+import ReactDOM from 'react-dom'
+import App from './App'
+
+class View extends App {
+    render(){
+    <>
+        <h1>Hello</h1>
+    </>
+    }
+}
+
+
+
+export default View; 
