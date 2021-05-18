ba9db31d26019c91416837a45b5a799670657479
Mariusz Bialoszewski <m.p.bialoszewski@outlook.com>
2021-05-18 10:37:31 +0100

    Fixed navbar and scrolling animations

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,11 +2,12 @@ import React from "react"
 //ReactFontAwesome imports
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {faBars} from '@fortawesome/free-solid-svg-icons'
+import {Link} from 'react-scroll'
 
 const Navbar = () => {
     return (
       <>
-        <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+        <nav className="navbar fixed-top navbar-expand-lg navbar-light bg-dark">
         <div className="container">
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
           <FontAwesomeIcon icon={faBars} style={{color: 'var(--cvOrange)'}}/>
@@ -15,19 +16,19 @@ const Navbar = () => {
         <div className="collapse navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav ms-auto">
             <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only"></span></a>
+              <Link smooth={false} to="home" className="nav-link" href="#">Home <span className="sr-only"></span></Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">about me </a>
+              <Link smooth={false} to="about" className="nav-link" href="#">about me </Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Stack</a>
+              <Link smooth={false} to="services" className="nav-link" href="#">Stack</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">Experience</a>
+              <Link smooth={false} to="experience" className="nav-link" href="#">Experience</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link" href="#">GitHub</a>
+              <Link smooth={false} to="projects" className="nav-link" href="#">Projects</Link>
             </li>
             
           </ul>
