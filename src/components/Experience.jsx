ffa26bb226b754cb5d3fcf682619a8834b336186
Mariusz Bialoszewski <m.p.bialoszewski@outlook.com>
2021-05-18 01:21:24 +0100

    Experience component

---
+++ src/components/Experience.jsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+const Experience = () => {
+  return (
+    <div className="experience">
+      <div className="d-flex justify-content-center my-5">
+        <h1>experience</h1>
+      </div>
+      <div className="container experience-wrapper">
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>June 2020 - June 2021</h3>
+            <h4>Ruby on Rails Developer (Industry Placement)</h4>
+            <h5>Superbyte, Shrewsbury, UK</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>Febuary 2020- June 2020</h3>
+            <h4>Demonstrator</h4>
+            <h5>Aberystwyth University</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div>
+        {/* - */}
+        <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>January 2019-December 2019</h3>
+            <h4>Project Manager</h4>
+            <h5>FP Global Limited, Hong Kong (Remote)</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div>
+        {/* - */}
+        {/* <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>January 2015 - August 2015</h3>
+            <h4>Internation Sales Manager</h4>
+            <h5>Fild.net, Bialystok, Poland</h5>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div> */}
+        {/* - */}
+        {/* <div className="timeline-block timeline-block-right">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2017-2018</h3>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div> */}
+        {/* - */}
+        {/* <div className="timeline-block timeline-block-left">
+          <div className="marker"></div>
+          <div className="timeline-content">
+            <h3>2018-2020</h3>a
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sit omnis laboriosam odio deserunt reprehenderit optio cum ratione sequi pariatur deleniti iste perspiciatis officiis minima iusto hic, ipsa ea dicta velit! Quos, placeat ratione. Sunt cumque delectus asperiores et quas qui.</p>
+          </div>
+        </div> */}
+      </div>
+    </div>
+  )
+}
+
+export default Experience;
