35b820cacd777b6175ab9a1f1ccca234fe702ad9
Mariusz Bialoszewski <m.p.bialoszewski@outlook.com>
2021-05-18 12:29:04 +0100

    Adding Portfolio

---
+++ src/components/Portfolio.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Portfolio = () => {
+  return (
+    <div>
+      <div id='projects' className="div">
+        <h1>My Projects</h1>
+      </div>
+    </div>
+  )
+}
+
+export default Portfolio
